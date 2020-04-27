71c59ba21e56e627dc37e1960e6fa4289fc6ac7d
Antonio Murcia Alonso <antonio.murcia@iesdoctorbalmis.com>
2020-04-27 21:43:52 +0200

    Primer comit

---
+++ Ejercicio1/leerFicheroXML.js
@@ -0,0 +1,14 @@
+function gestionarFicheroXML(xmlDoc){
+	alert("llego");
+	//alert(xmlDoc.getElementsByTagName('alumno')[0].firstChild.nodeValue)
+	//alert(xmlDoc.getElementsByTagName('alumnos')[0].childNodes[1].firstChild.nodeValue)
+
+	//document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('alumno:nth-child(2)').textContent + "</b>"
+	
+}
+
+//document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
+//	document.querySelector("#botonXML").addEventListener("click",()=>{
+window.addEventListener("load", function(event) {
+    let xmlDoc = loadLDocA("libros.xml","xml");
+})
