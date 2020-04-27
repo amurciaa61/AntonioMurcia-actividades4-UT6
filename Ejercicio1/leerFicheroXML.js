feb63fad40253d992e651a1375c184fd0e2f02b2
Antonio Murcia Alonso <antonio.murcia@iesdoctorbalmis.com>
2020-04-27 22:29:17 +0200

    pruabas

--- Ejercicio1/leerFicheroXML.js
+++ Ejercicio1/leerFicheroXML.js
@@ -2,9 +2,17 @@ function gestionarFicheroXML(xmlDoc){
 	alert("llego");
 	alert(xmlDoc.getElementsByTagName('titulo')[0].firstChild.nodeValue)
 	alert("llego");
+	let texto = "<table><tr><th>Titulo</th><th>Autor</th></tr>";
+	let titulos = xmlDoc.querySelector('titulo').textContent;
+	let autores = xmlDoc.querySelector('autor').textContent;
+	let precios = xmlDoc.querySelector('precio').textContent;
+	for (i=0;i<titulos.length;i++){
+		texto = texto + "<dr>" + titulos[i] + "</dr>";
+	}
 	//alert(xmlDoc.getElementsByTagName('alumnos')[0].childNodes[1].firstChild.nodeValue)
 
-	//document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('alumno:nth-child(2)').textContent + "</b>"
+	texto = texto + "</table>";
+	//document.getElementById('tablalibros').innerHTML += "<b>" + xmlDoc.querySelector('alumno:nth-child(2)').textContent + "</b>"
 	
 }
 
