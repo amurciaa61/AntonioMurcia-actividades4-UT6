31898e55f2ba9bff72dbd2f830f1d35c5bbb8759
Antonio Murcia Alonso <antonio.murcia@iesdoctorbalmis.com>
2020-04-28 14:36:15 +0200

    Primer commit

---
+++ Ejercicio2/leerFicheroTXT.js
@@ -0,0 +1,16 @@
+function gestionarFicheroTXT(txtDoc){
+    let contador = 0;
+    let registros = txtDoc.split(/\r?\n|\r/);
+    for (let i = 0; i < registros.length; i++) 
+    { 
+        let contenido = "";
+        let campos = registros[i].split(";");
+        contenido = "<p class=\"" + campos[0]+ "\">" + campos[1] + "</p>";
+        document.getElementById('ficheroTXT').innerHTML = contenido;
+    }
+}
+let puntero = 0;
+let intervalo = setInterval(leerfichero, 3000);
+function leerfichero() {
+    let txtDoc = loadLDocA("leerFicheroTXT.txt","txt");
+}
