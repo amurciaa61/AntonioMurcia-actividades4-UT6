3f0175268afc9d910877b007ade73cae2db8d6e7
Antonio Murcia Alonso <antonio.murcia@iesdoctorbalmis.com>
2020-04-28 14:56:09 +0200

    correccion en css y acumulador de parrafos

--- Ejercicio2/leerFicheroTXT.js
+++ Ejercicio2/leerFicheroTXT.js
@@ -1,9 +1,9 @@
 function gestionarFicheroTXT(txtDoc){
     let contador = 0;
     let registros = txtDoc.split(/\r?\n|\r/);
+    let contenido = "";
     for (let i = 0; i < registros.length; i++) 
     { 
-        let contenido = "";
         let campos = registros[i].split(";");
         contenido += "<p class=\"" + campos[0]+ "\">" + campos[1] + "</p>";
         document.getElementById('ficheroTXT').innerHTML = contenido;
